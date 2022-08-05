88e496530f92a14496cdc3a57bfab478ec445170
Krishna Kumar Mandal <krishiway@gmail.com>
2022-08-05 22:44:27 +0200

    stage 1 done

--- Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric Matrix Processor/task/src/processor/Main.kt
@@ -1,5 +1,32 @@
 package processor
 
+import java.util.Arrays
+
 fun main() {
-    println("Hello World!")
+
+    val (row1, col1) = readln().split(" ").map { it.toInt() }
+
+    val mat1 = Array(row1) {
+            readln().split(" ").map { it.toInt() }
+    }
+
+    val (row2, col2) = readln().split(" ").map { it.toInt() }
+
+    val mat2 = Array(row2) {
+            readln().split(" ").map { it.toInt() }
+    }
+
+    if (row1 != row2 && col1 != col2) {
+        println("ERROR")
+    } else {
+        for (row in 0 until row1) {
+            for (col in 0 until col1) {
+                print(mat1[row][col] + mat2[row][col])
+                if (col != col1 - 1) {
+                    print(" ")
+                }
+            }
+            println()
+        }
+    }
 }
